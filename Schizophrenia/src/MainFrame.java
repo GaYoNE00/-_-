82438666e5463224b75b04ae59734fb41c0be189
gayone <201945023@itc.ac.kr>
2023-12-04 13:44:24 +0900

    고객 요구사항 반영

--- Schizophrenia/src/MainFrame.java
+++ Schizophrenia/src/MainFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MainFrame extends JFrame implements KeyListener {
-
+    int a = 0;
     private JPanel pn;
     private JPanel pn_2;
     private Timer fadeOutTimer, fadeInTimer;
@@ -20,12 +22,12 @@ public class MainFrame extends JFrame implements KeyListener {
 
     private int currentScreen = 1; // 현재 화면을 추적하는 변수
     // 대만어 트리거 단어와 중국어 문장 배열
-    private String[] triggerWords = {"很好","幸福","媽媽", "爸爸", "母親", "父親", "哥哥", "弟弟", "兄弟", "姐妹", "姐姐", "妹妹", "家庭", "朋友", "熟人", "我", "我的"};
+    private String[] triggerWords = {"很好","幸福","媽媽", "爸爸", "母親", "父親", "哥哥", "弟弟", "兄弟", "姐妹", "姐姐", "妹妹", "家庭", "朋友", "熟人", "我的"};
     private String[] randomChineseStrings = {"遲早會死的", "前面那個男人在看著我", "蔡秀琴，王志明", "賤人", "一直把垃圾塞進嘴裡", "剛剛旁邊那女人在嘲笑我", "醜男", "醜女", "用刀殺掉他吧", "你是故意的吧", "想死", "好髒"};
     private String text;
     private ArrayList<String> triggerList = new ArrayList<String>();
     private int ac = 0;
-
+    private Timer randomStringTimer; // 새로운 Timer 객체 추가
     public MainFrame(String title) {
         setTitle(title);
 //        setSize(1920, 1080);
@@ -49,37 +51,42 @@ public class MainFrame extends JFrame implements KeyListener {
         pn.setLayout(new BorderLayout());
         pn.setBackground(Color.white);
 
-        JLabel lblTitle = new JLabel("你應該要快樂的時刻 / The Moment When Should Be Happy");
-        lblTitle.setFont(new Font("ToneOZ-Zhuyin-Kai-Traditional", Font.BOLD, 20));
-        lblTitle.setBorder(new EmptyBorder(350, 300, 0, 50));
+        JLabel lblTitle = new JLabel("你應該要快樂的時刻");
+        lblTitle.setFont(new Font("YangRenDongZhuShiTi", Font.TRUETYPE_FONT, 70));
+        lblTitle.setBorder(new EmptyBorder(700, 600, 0, 0));
+
+        JLabel lblTitle2 = new JLabel("按'Enter'开始。");
+        lblTitle2.setFont(new Font("YangRenDongZhuShiTi", Font.TRUETYPE_FONT, 70));
+        lblTitle2.setBorder(new EmptyBorder(0, 700, 100, 50));
+        lblTitle2.setForeground(Color.gray);
 
         JTextArea tra = new JTextArea();
         tra.setEditable(false);
-        tra.setText("你應該要快樂的時刻>代表了即使是我最快樂的記憶，\n也可能會因為罵人的話、恐懼和憤怒而被歪曲這是一種直接的無力感，\n我的記憶被扭曲了 \n我希望你能夠想像並且情感上參與這個過程，\n記憶中原本應該是快樂的時刻變得不穩定和曲解");
-        tra.setFont(new Font("ToneOZ-Zhuyin-Kai-Traditional", Font.BOLD, 20));
-        tra.setBorder(new EmptyBorder(100, 300, 0, 00));
+        tra.setFont(new Font("YangRenDongZhuShiTi", Font.TRUETYPE_FONT, 20));
+        tra.setBorder(new EmptyBorder(0, 0, 0, 0));
 
         pn.add(lblTitle, BorderLayout.NORTH);
+        pn.add(lblTitle2, BorderLayout.SOUTH);
         pn.add(tra, BorderLayout.CENTER);
 
         add(pn, BorderLayout.CENTER);
     }
 
     public void SecondScreen() {
+
         currentScreen = 2; // 두 번째 화면으로 설정
-        JPanel pn_2_center = new JPanel();
+//        JPanel pn_2_center = new JPanel();
         JPanel pn_2_right = new JPanel();
         JPanel pn_2_left = new JPanel();
         JPanel pn_2_top = new JPanel();
         JPanel pn_2_bottom = new JPanel();
         pn_2_right.setBackground(Color.black);
-        pn_2_left.setBackground(Color.black);
         pn_2_top.setBackground(Color.black);
         pn_2_bottom.setBackground(Color.black);
-        pn_2_center.setBackground(Color.black);
-        pn_2_center.setBorder(new EmptyBorder(90, 403, 0, 403));
+//        pn_2_center.setBackground(Color.black);
+//        pn_2_center.setBorder(new EmptyBorder(90, 403, 0, 403));
 
-        pn_2_center.setLayout(new BorderLayout());
+//        pn_2_center.setLayout(new BorderLayout());
 
         pn_2 = new TransparentPanel();
         pn_2.setLayout(new BorderLayout());
@@ -89,55 +96,82 @@ public class MainFrame extends JFrame implements KeyListener {
         pns_2.setBorder(new EmptyBorder(50, 50, 0, 50));
         pns_2.setBackground(Color.white);
 
+        JTextArea title = new JTextArea("你人生中最幸福的那一天是怎樣的一天呢? \n\r 是什麼樣的記憶使你那天覺得幸福呢?");
+        title.setEditable(false);
+        title.setFont(new Font("YangRenDongZhuShiTi", Font.TRUETYPE_FONT, 30));
+        title.setBorder(new EmptyBorder(50, 600, 0, 50));
+
+        JTextField titleBottom = new JTextField("请按f11重置。");
+        titleBottom.setFont(new Font("YangRenDongZhuShiTi", Font.TRUETYPE_FONT, 30));
+        titleBottom.setBorder(new EmptyBorder(50, 1500, 0, 50));
+        titleBottom.setForeground(Color.gray);
+
         JTextPane lblTitle_2 = new JTextPane();
-        StyledDocument doc = lblTitle_2.getStyledDocument();
+//        StyledDocument doc = lblTitle_2.getStyledDocument();
         SimpleAttributeSet center = new SimpleAttributeSet();
         StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
 
-        try {
-            doc.insertString(doc.getLength(), "你人生中最幸福的那一天是怎樣的一天呢? 是什麼樣的記憶使你那天覺得幸福呢?", null);
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
-        doc.setParagraphAttributes(0, doc.getLength(), center, false);
-        lblTitle_2.setFont(new Font("HY신명조", Font.BOLD, 30));
-        lblTitle_2.setBorder(new EmptyBorder(150, 100, 0, 50));
+//        try {
+//            doc.insertString(doc.getLength(), "你人生中最幸福的那一天是怎樣的一天呢? \n 是什麼樣的記憶使你那天覺得幸福呢?", null);
+//        } catch (BadLocationException e) {
+//            e.printStackTrace();
+//        }
+//        doc.setParagraphAttributes(0, doc.getLength(), center, false);
+//        lblTitle_2.setFont(new Font("HanZiZhiMeiFangSongGBK", Font.BOLD, 30));
+//        lblTitle_2.setBorder(new EmptyBorder(100, 100, 0, 50));
 
-        txt_2 = new JTextArea("< 慢慢地, 在這裡輸入");
+        txt_2 = new JTextArea();
+
         txt_2.setForeground(Color.GRAY);
-        txt_2.setFont(new Font("바탕", Font.BOLD, 30));
+        txt_2.setFont(new Font("YangRenDongZhuShiTi", Font.TRUETYPE_FONT, 30));
         txt_2.setLineWrap(true);
+//        txt_2.setBackground(Color.black);
         txt_2.setWrapStyleWord(true);
-        txt_2.addFocusListener(new FocusAdapter() {
+        randomStringTimer = new Timer(20000, new ActionListener() {
+            private boolean isFirstTime = true;
             @Override
-            public void focusGained(FocusEvent e) {
-                if (txt_2.getText().equals("< 慢慢地, 在這裡輸入")) {
-                    txt_2.setText("");
-                    txt_2.setForeground(Color.BLACK);
-                }
-            }
+            public void actionPerformed(ActionEvent e) {
+                randomStringTimer.setDelay(10000); // 첫 실행 후 8초로 변경
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (txt_2.getText().isEmpty()) {
-                    txt_2.setForeground(Color.GRAY);
-                    txt_2.setText("< 慢慢地, 在這裡輸入");
-                }
+
+                String randomStr = randomChineseStrings[new Random().nextInt(randomChineseStrings.length)];
+                txt_2.append(randomStr); // txt_2에 문자열 추가
             }
         });
+        randomStringTimer.start();
+//
+//        txt_2.addFocusListener(new FocusAdapter() {
+//            @Override
+//            public void focusGained(FocusEvent e) {
+//                if (txt_2.getText().equals("< 慢慢地, 在這裡輸入")) {
+//                    txt_2.setText("");
+//                    txt_2.setForeground(Color.BLACK);
+//                }
+//            }
+//
+//            @Override
+//            public void focusLost(FocusEvent e) {
+//                if (txt_2.getText().isEmpty()) {
+//                    txt_2.setForeground(Color.GRAY);
+//                    txt_2.setText("< 慢慢地, 在這裡輸入");
+//                }
+//            }
+//        });
 
         txt_2.addKeyListener(this);
 
         pns_2.add(lblTitle_2, BorderLayout.NORTH);
         pns_2.add(txt_2, BorderLayout.CENTER);
 
-        pn_2_center.add(lblTitle_2, BorderLayout.NORTH);
-        pn_2_center.add(pns_2, BorderLayout.CENTER);
-        pn_2.add(pn_2_right, BorderLayout.EAST);
-        pn_2.add(pn_2_left, BorderLayout.WEST);
-        pn_2.add(pn_2_top, BorderLayout.NORTH);
-        pn_2.add(pn_2_bottom, BorderLayout.SOUTH);
-        pn_2.add(pn_2_center, BorderLayout.CENTER);
+        pn_2.add(title,BorderLayout.NORTH);
+//        pn_2.add(lblTitle_2, BorderLayout.NORTH);
+        pn_2.add(pns_2, BorderLayout.CENTER);
+        pn_2.add(titleBottom, BorderLayout.SOUTH);
+//        pn_2.add(pn_2_right, BorderLayout.EAST);
+//        pn_2.add(pn_2_left, BorderLayout.WEST);
+//        pn_2.add(pn_2_top, BorderLayout.NORTH);
+//        pn_2.add(pn_2_bottom, BorderLayout.SOUTH);
+//        pn_2.add(pn_2_center, BorderLayout.CENTER);
         pn_2.setOpaque(false);
         alpha = 0f;
 
@@ -157,6 +191,9 @@ public class MainFrame extends JFrame implements KeyListener {
             }
         });
         fadeInTimer.start();
+        txt_2.grabFocus();
+        txt_2.requestFocus();
+
     }
 
     class TransparentPanel extends JPanel {
@@ -170,29 +207,18 @@ public class MainFrame extends JFrame implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        char c = e.getKeyChar();
-        text += c; // 사용자가 타이핑한 문자를 현재 텍스트에 추가
 
-        // 트리거 단어 감지 및 처리
-        for (String trigger : triggerWords) {
-            if (text.contains(trigger)) {
-                String randomStr = randomChineseStrings[new Random().nextInt(randomChineseStrings.length)];
-                text += " " + randomStr;
-                txt_2.setText(text);
-                txt_2.setCaretPosition(text.length());
-                text = ""; // 텍스트 초기화
-                break;
-            }
-        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
+        System.out.println("키입력2");
+
         if (currentScreen == 1 && e.getKeyCode() == KeyEvent.VK_ENTER) {
             // 첫 번째 화면에서 엔터 키를 눌렀을 때
             // ... 첫 번째 화면에서 두 번째 화면으로 전환하는 코드 ...
 
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+
             System.out.println("Enter key");
             fadeOutTimer = new Timer(15, new ActionListener() {
                 @Override
@@ -208,65 +234,24 @@ public class MainFrame extends JFrame implements KeyListener {
                 }
             });
             fadeOutTimer.start();
-        }}else if(currentScreen == 2){
-            text = txt_2.getText();
-            for (String a : triggerList){
-                System.out.println("이것은 a"+a);
-                text = text.replace(a,"");
-                System.out.println("리스트 안"+triggerList);
-            }
-            for (String trigger : triggerWords) {
-                if (text.contains(trigger)) {
-                    triggerList.add(trigger);
-//                    String randomStr = randomChineseStrings[new Random().nextInt(randomChineseStrings.length)];
-//                    txt_2.setText(txt_2.getText() + " " + randomStr);
-//                    addTextOneByOne(" " + randomStr, 50);
-                    if(text.contains("媽媽")||text.contains("母親")){
-                        addTextOneByOne(" " + "覺得我不應該出生。", 50);
-                    }else if(text.contains("爸爸")||text.contains("父親")){
-                        addTextOneByOne(" " + "希望我死", 50);
-                    }else if(text.contains("女朋友")){
-                        addTextOneByOne(" " + "醜女", 50);
-                    }else if(text.contains("男朋友")){
-                        addTextOneByOne(" " + "醜男", 50);
-                    }else if(text.contains("愛人")){
-                        addTextOneByOne(" " + "用刀殺掉他吧", 50);
-                    }else if(text.contains("家庭")||text.contains("眷屬")){
-                        addTextOneByOne(" " + "討厭我", 50);
-                    }else if(text.contains("老太太 ")||text.contains("爺爺")){
-                        addTextOneByOne(" " + "覺得我不是親生子女。", 50);
-                    }else if(text.contains("姐姐")||text.contains("姐兒")){
-                        addTextOneByOne(" " + "蔡秀琴", 50);
-                    }else if(text.contains("姐妹")||text.contains("妹妹")){
-                        addTextOneByOne(" " + "賤人", 50);
-                    }else if(text.contains("哥哥")||text.contains("荊")){
-                        addTextOneByOne(" " + "王志明", 50);
-                    }else if(text.contains("弟弟")||text.contains("兄弟")){
-                        addTextOneByOne(" " + "遲早會死。", 50);
-                    }else if(text.contains("朋友")){
-                        String r[] = {"一直把垃圾塞進嘴裡","後面那個男人在看着我。","剛剛旁邊那女人在嘲笑我 ", "好髒"};
-                        if(ac<4){
-                         ac +=1;
-                        }else{
-                         ac = 0;
-                        }
-                        addTextOneByOne(" " + r[ac], 50);
-
-                    }else if(text.contains("幸福")){
-                        String gt = txt_2.getText().replace("幸福","不幸");
-                        txt_2.setText(gt);
-                    }else if(text.contains("很好")){
-                        String gt = txt_2.getText().replace("很好","不好");
-                        txt_2.setText(gt);
-                    }
-                    txt_2.setCaretPosition(txt_2.getText().length());
-                    System.out.println(text);
-                    text = ""; // 텍스트 초기화
-                    break;
-                }
-            }
+        }else if(currentScreen == 2){
+//            text = txt_2.getText();
+//            for (String a : triggerList){
+//                System.out.println("이것은 a"+a);
+//                text = text.replace(a,"");
+//                System.out.println("리스트 안"+triggerList);
+//            }
+//            for (String trigger : triggerWords) {
+//                if (text.contains(trigger)) {
+//                    triggerList.add(trigger);
+////                    String randomStr = randomChineseStrings[new Random().nextInt(randomChineseStrings.length)];
+////                    txt_2.setText(txt_2.getText() + " " + randomStr);
+////                    addTextOneByOne(" " + randomStr, 50);
+//
+//                }
+//            }
         }
-        if (e.getKeyCode() == KeyEvent.VK_F10) {
+        if (e.getKeyCode() == KeyEvent.VK_F11) {
             // 초기화해야 할 모든 변수들을 초기 상태로 되돌립니다.
             resetApplication();
         }
